e15eeb9d2b20016bc0241bd88be522ae48b4ec5a
Thore Goebel <git@thore.io>
2026-09-05 15:21:55 +0200

    fix: Correctly handle multipart SMS
    Combine the PDUs into one message

--- app/src/main/java/de/nulide/findmydevice/receiver/SmsReceiver.kt
+++ app/src/main/java/de/nulide/findmydevice/receiver/SmsReceiver.kt
@@ -31,39 +31,47 @@ class SmsReceiver : BroadcastReceiver() {
         val pdus = bundle.get("pdus") as Array<ByteArray>? ?: return
         val subscriptionId = intent.getIntExtra("subscription", -1)
 
+        var phoneNumber: String? = null
+        var fullMessageBuilder = StringBuilder()
+
+        // Long, multipart SMS: combine into one message
         for (pdu in pdus) {
             val sms = SmsMessage.createFromPdu(pdu, format)
-
-            var phoneNumber = sms.originatingAddress
-            if (phoneNumber.isNullOrBlank()) {
-                context.log().i(TAG, "Cannot handle SMS: phoneNumber is empty!")
-                return
+            if (phoneNumber == null) {
+                phoneNumber = sms.originatingAddress
             }
-            phoneNumber = normalizePhoneNumber(context, phoneNumber) ?: phoneNumber
-
-            val msg = sms.messageBody
-            if (msg.isNullOrBlank()) {
-                context.log().i(TAG, "Cannot handle SMS: msg is empty!")
-                return
-            }
-
-            // Early sanity check + abort
-            val settings = SettingsRepository.getInstance(context)
-            val fmdTriggerWord = (settings.get(Settings.SET_FMD_COMMAND) as String).lowercase()
-            if (!msg.lowercase().startsWith(fmdTriggerWord)) {
-                return
-            }
-
-            val inputData = workDataOf(
-                CommandExecutionWorker.KEY_COMMAND to msg,
-                CommandExecutionWorker.KEY_TRANSPORT_TYPE to CommandExecutionWorker.TRANS_SMS,
-                CommandExecutionWorker.KEY_DESTINATION to phoneNumber,
-                CommandExecutionWorker.KEY_SMS_SUBSCRIPTION_ID to subscriptionId,
-            )
-            val workRequest = OneTimeWorkRequestBuilder<CommandExecutionWorker>()
-                .setInputData(inputData)
-                .build()
-            WorkManager.getInstance(context).enqueue(workRequest)
+            fullMessageBuilder.append(sms.messageBody)
         }
+
+        if (phoneNumber.isNullOrBlank()) {
+            context.log().i(TAG, "Cannot handle SMS: phoneNumber is empty!")
+            return
+        }
+        phoneNumber = normalizePhoneNumber(context, phoneNumber) ?: phoneNumber
+
+        val msg = fullMessageBuilder.toString()
+
+        if (msg.isBlank()) {
+            context.log().i(TAG, "Cannot handle SMS: msg is empty!")
+            return
+        }
+
+        // Early sanity check + abort
+        val settings = SettingsRepository.getInstance(context)
+        val fmdTriggerWord = (settings.get(Settings.SET_FMD_COMMAND) as String).lowercase()
+        if (!msg.lowercase().startsWith(fmdTriggerWord)) {
+            return
+        }
+
+        val inputData = workDataOf(
+            CommandExecutionWorker.KEY_COMMAND to msg,
+            CommandExecutionWorker.KEY_TRANSPORT_TYPE to CommandExecutionWorker.TRANS_SMS,
+            CommandExecutionWorker.KEY_DESTINATION to phoneNumber,
+            CommandExecutionWorker.KEY_SMS_SUBSCRIPTION_ID to subscriptionId,
+        )
+        val workRequest = OneTimeWorkRequestBuilder<CommandExecutionWorker>()
+            .setInputData(inputData)
+            .build()
+        WorkManager.getInstance(context).enqueue(workRequest)
     }
 }
